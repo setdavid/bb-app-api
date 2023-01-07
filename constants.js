255f841345ee0ccea45c4fc81c16a2b5b033abc4
David Setiawan <setdavid1014@gmail.com>
2023-01-06 19:42:37 -0500

    added refresh access token method

--- constants.js
+++ constants.js
@@ -1,5 +1,6 @@
 // const HOST_URL = "http://localhost:8080"
+const EXPIRES_IN = 30 //measured in seconds
 
 module.exports = {
-
+    EXPIRES_IN
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,42 @@
 const express = require('express')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const { USERS } = require('../constants')
+const { EXPIRES_IN } = require('../constants')
 const router = express.Router()
 require('dotenv').config()
 
+const generateAccessToken = (user) => {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: `${EXPIRES_IN}s` })
+}
+
+const authenticateAccessToken = (req, res, next) => {
+    const authHeader = req.headers["authorization"]
+    const accessToken = authHeader ? authHeader.split(" ")[1] : null
+
+    if (accessToken == null) {
+        res.status(400).json({ error: "MISSING_ACCESS_TOKEN" })
+    } else {
+        jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+            if (err) {
+                if (err.name == "TokenExpiredError") {
+                    res.status(401).json({ error: "EXPIRED_ACCESS_TOKEN" })
+                } else {
+                    res.status(401).json({ error: "INVALID_ACCESS_TOKEN" })
+                }
+            } else {
+                req.user = user
+                next()
+            }
+        })
+    }
+}
+
 //only used to generate hashed passwords!
-router.get("/generate-hashed-password", async (req, res) => {
-    const { password } = req.query
+router.post("/generate-hashed-password", async (req, res) => {
+    const { password } = req.body
 
     try {
         const hashedPassword = await bcrypt.hash(password, 10)
-        console.log(hashedPassword)
         res.status(200).json({ hashedPassword })
     } catch (err) {
         console.log(err);
@@ -19,8 +44,8 @@ router.get("/generate-hashed-password", async (req, res) => {
     }
 })
 
-router.get("/login", async (req, res) => {
-    const { username, password } = req.query
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body
     let hashedPassword = null;
 
     if (username == process.env.USERNAME_1) {
@@ -39,8 +64,9 @@ router.get("/login", async (req, res) => {
         try {
             if (await bcrypt.compare(password, hashedPassword)) {
                 const user = { username }
-                const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-                res.status(200).json({ access_token: accessToken })
+                const accessToken = generateAccessToken(user)
+                const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+                res.status(200).json({ access_token: accessToken, refresh_token: refreshToken, expires_in: EXPIRES_IN })
             } else {
                 res.status(401).json(failAuthError)
             }
@@ -51,26 +77,29 @@ router.get("/login", async (req, res) => {
     }
 })
 
-const authenticateAccessToken = (req, res, next) => {
-    const authHeader = req.headers["authorization"]
-    console.log(req.headers)
-    const accessToken = authHeader ? authHeader.split(" ")[1] : null
+router.post("/refresh", (req, res) => {
+    const refreshToken = req.body.refresh_token
 
-    if (accessToken == null) {
-        res.status(401).json({ error: "MISSING_ACCESS_TOKEN" })
+    if (refreshToken == null) {
+        res.status(400).json({ error: "MISSING_REFRESH_TOKEN" })
     } else {
-        jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
             if (err) {
-                res.status(403).json({ error: "INVALID_ACCESS_TOKEN" })
+                if (err.name == "TokenExpiredError") {
+                    res.status(401).json({ error: "EXPIRED_REFRESH_TOKEN" })
+                } else {
+                    res.status(401).json({ error: "INVALID_REFRESH_TOKEN" })
+                }
             } else {
-                req.user = user
-                next()
+                const userObj = { username: user.username }
+                const accessToken = generateAccessToken(userObj)
+                res.status(200).json({ access_token: accessToken, refresh_token: refreshToken, expires_in: EXPIRES_IN })
             }
         })
     }
-}
+})
 
-router.get("/test", authenticateAccessToken, async (req, res) => {
+router.post("/test", authenticateAccessToken, async (req, res) => {
     res.json(req.user)
 })
 
